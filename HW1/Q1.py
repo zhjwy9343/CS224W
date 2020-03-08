0376a64c2880d0e2df3bcf0b74fa58d6efd68ce4
james <6593865@qq.com>
2020-03-08 16:16:14 +0800

    HW 1 part 1 two graphs created

--- HW1/Q1.py
+++ HW1/Q1.py
@@ -3,6 +3,11 @@
 # Author:james Zhang
 # Datetime:20-3-8 上午10:34
 # Project: CS224W
+"""
+    This version is purely based on python library without use SNAP
+
+    SNAP vesion cuold be found in the hw1-q1-starter.py file
+"""
 
 
 import snap
@@ -24,7 +29,7 @@ class ER_graph(object):
         self.e = e
 
         # sanity check
-        if (e > (n * n / 2)):
+        if (n < 0) or (e < 0) or (e > (n * n / 2)):
             raise Exception("The given number of edges {} is greater than theorical upper limit".format(e))
 
         # call method
@@ -79,18 +84,73 @@ class small_world_graph(object):
         self.n = n
         self.e = e
 
-        if (e > (n * n / 2)):
+        # sanity check
+        if (n < 0) or (e < 0) or (e > (n * n / 2)):
             raise Exception("The given number of edges {} is greater than theorical upper limit".format(e))
+
+        if (e < 2 * n):
+            raise Exception('The number of edges is not big enough for this method')
 
         self.graph = self._generate_graph()
 
     def _generate_graph(self):
-        pass
+
+        # get node list with given size
+        node_list = np.arange(self.n)
+        print(node_list[:10])
+
+        # edge with neighbors, use (i + n +-1) % n to get neighbors
+        # SNAP undirected graph can detect duplicate edges, but here I will use
+        edge_set = set()
+        for node in node_list:
+            dst_p1 = (node + self.n + 1) % self.n
+            dst_n1 = (node + self.n - 1) % self.n
+
+            dst_p2 = (node + self.n + 2) % self.n
+            dst_n2 = (node + self.n - 2) % self.n
+
+            if not (dst_p1, node) in edge_set:
+                edge_set.add((node, dst_p1))
+            if not (dst_n1, node) in edge_set:
+                edge_set.add((node, dst_n1))
+            if not (dst_p2, node) in edge_set:
+                edge_set.add((node, dst_p2))
+            if not (dst_n2, node) in edge_set:
+                edge_set.add((node, dst_n2))
+
+        rest_edges = self.e - len(edge_set)
+
+        count = 0
+        while count < rest_edges:
+            rand_pair = np.random.random_integers(low=0, high=self.n, size=2)
+            if (rand_pair[0] != rand_pair[1]) and (not ((rand_pair[0], rand_pair[1]) in edge_set)):
+                edge_set.add((rand_pair[0], rand_pair[1]))
+                count += 1
+
+        # create SNAP graph
+        graph = snap.TUNGraph.New()
+
+        for node in node_list.tolist():
+            graph.AddNode(node)
+
+        print(len(list(graph.Nodes())))
+
+        # Have to complain that SNAP is sooooooo wierd in its data type! has to use int() to convert a variable so that
+        # the AddEdge fn can work. Tooooo noisy!
+        for edge in list(edge_set):
+            src = int(edge[0])
+            dst = int(edge[1])
+            graph.AddEdge(src, dst)
+
+        print(len(list(graph.Edges())))
+        return graph
 
 
 if __name__ == '__main__':
-    g = ER_graph(n=5242, e=14484)
+    g_ER = ER_graph(n=5242, e=14484)
 
     # SNAP implementation of ER graph is much faster than my naive version
     # g1 = snap.GenRndGnm(snap.PUNGraph, 5242, 14484)
     # print(len(list(g1.Nodes())))
+
+    g_SW = small_world_graph(n=5242, e=14484)
